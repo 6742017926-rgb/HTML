38f006d38415fc985f4cabfe27cc208bbbbe02fa
6742017926-rgb <6742017926@student.chula.ac.th>
2026-03-12 09:26:12 +0000

    update

--- members/admin.py
+++ members/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Member
 
-admin.site.register(Member)
-# Register your models here.
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ("firstname", "lastname", "joined_date")
+
+
+admin.site.register(Member, MemberAdmin)
